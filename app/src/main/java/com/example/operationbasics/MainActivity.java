c5487e85699ee03bf55d346bc88d04426dc76aef
dwru <dwightrussel15@gmail.com>
2022-10-17 22:46:36 +0800

    AMOGUS

--- app/src/main/java/com/example/operationbasics/MainActivity.java
+++ app/src/main/java/com/example/operationbasics/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.operationbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.os.Bundle; //
 
 public class MainActivity extends AppCompatActivity {
-
+    //AMOGUS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
