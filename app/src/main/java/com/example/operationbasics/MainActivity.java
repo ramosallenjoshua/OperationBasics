fd30a560f27761366a111e63400c801e84ae3877
AllenRamos <kazutofujiwara09@gmail.com>
2022-11-08 13:32:38 +0800

    2nd commit -Proper development initiation Changes: -Added "test" and "statistics" activities -Modified main activity for activity transition -Added corresponding buttons (not final design) for activity transition

--- app/src/main/java/com/example/operationbasics/MainActivity.java
+++ app/src/main/java/com/example/operationbasics/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.operationbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle; //
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView testButton;
+    ImageView assessButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testButton = findViewById(R.id.testButton);
+        assessButton = findViewById(R.id.assesButton);
+
+        testButton.setOnClickListener(View ->{
+            Intent intent = new Intent(getApplicationContext(), MathProblemsActivity.class);
+            startActivity(intent);
+        });
+
+        assessButton.setOnClickListener(View ->{
+            Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
+            startActivity(intent);
+        });
     }
 }
