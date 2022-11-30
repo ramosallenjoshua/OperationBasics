2fd653f22a3582ce60dcb4b996326825e4548e9e
AllenRamos <kazutofujiwara09@gmail.com>
2022-11-30 17:09:25 +0800

    6th commit 3 -WiP: MathProblemsActivity, AdditionFragment, and SubtractionFragment

--- app/src/main/java/com/example/operationbasics/AdditionFragment.java
+++ app/src/main/java/com/example/operationbasics/AdditionFragment.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class AdditionFragment extends Fragment{
 
+    int num1, num2, answer;
+    EditText additionAnswerEditText;
+    ImageView nextButton, quitButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        String question = num1 + " + " + num2 + " = ? ";
+
+
         return inflater.inflate(R.layout.fragment_addition, container, false);
     }
 }
