75a572281af40f2c5fc2411d86c95502a7f3c293
AllenRamos <kazutofujiwara09@gmail.com>
2022-12-05 14:29:55 +0800

    7th commit

--- app/src/main/java/com/example/operationbasics/LoginMenuActivity.java
+++ app/src/main/java/com/example/operationbasics/LoginMenuActivity.java
@@ -1,18 +1,24 @@
 package com.example.operationbasics;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginMenuActivity extends AppCompatActivity {
 
-
+    ImageView loginButton;
+    EditText emailEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_menu);
 
+        loginButton = findViewById(R.id.login_button);
+        emailEditText = findViewById(R.id.user_input_email);
+        passwordEditText = findViewById(R.id.user_input_password);
 
 
     }
