e67462ad635fdf7c9bc37b79f378c1615b7b0e84
AllenRamos <kazutofujiwara09@gmail.com>
2022-12-05 14:49:32 +0800

    7th commit

--- app/src/main/java/com/example/operationbasics/LoginMenuActivity.java
+++ app/src/main/java/com/example/operationbasics/LoginMenuActivity.java
@@ -6,20 +6,28 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginMenuActivity extends AppCompatActivity {
 
     ImageView loginButton;
     EditText emailEditText, passwordEditText;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_menu);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         loginButton = findViewById(R.id.login_button);
         emailEditText = findViewById(R.id.user_input_email);
         passwordEditText = findViewById(R.id.user_input_password);
 
+        loginButton.setOnClickListener(View->{
+            emailEditText.getText().toString();
+            passwordEditText.getText().toString();
+        });
 
     }
 }
--- app/src/main/java/com/example/operationbasics/MainActivity.java
+++ app/src/main/java/com/example/operationbasics/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.operationbasics;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
